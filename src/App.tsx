30726b465dc4eed70f5aa6c954a68c2c773d734d
Anton-Pan <91840673+Anton-Pan@users.noreply.github.com>
2025-07-17 15:50:08 -0700

    Created footer & componentized basic site elements - Added first names, class name and copyright year to footer - Deleted deleteMe.js placeholder in views folder - Componentized each site element

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import Globe from 'react-globe.gl';
 import "./App.css"
+import {Footer} from "./backend/view/footer.tsx"
+import {SiteContent} from "./backend/view/site-content.tsx";
 import {createTheme, ThemeProvider} from '@mui/material/styles';
 import {CssBaseline} from "@mui/material";
-import {useEffect, useState} from "react";
 
 const darkTheme = createTheme({
     palette: {
@@ -10,56 +10,16 @@ const darkTheme = createTheme({
     },
 });
 
-const GlobeContainer = () => {
-    /*
-        The Globe container only accepts width/height values in px,
-        so the width and height need to be recalculated manually
-        every time the user zooms.
-    */
-    const [dimensions, setDimensions] = useState({
-        width: window.innerWidth * 3 / 5,
-        height: window.innerHeight
-    });
-
-    useEffect(() => {
-        const handleResize = () => {
-            setDimensions({
-                width: window.innerWidth * 3 / 5,
-                height: window.innerHeight
-            });
-        };
-
-        // Whenever the user resizes the page, call the handleResize function
-        window.addEventListener('resize', handleResize);
-    }, []);
-
-    return (
-        <div>
-            <Globe
-                width={dimensions.width}
-                height={dimensions.height}
-                animateIn={true}
-                globeImageUrl="//cdn.jsdelivr.net/npm/three-globe/example/img/earth-night.jpg"
-            />
-        </div>
-    );
-};
-
 function App() {
     return (<>
             <ThemeProvider theme={darkTheme}>
                 <CssBaseline/>
-                    <div style={{
-                        display: "flex",
-                        flexDirection: "row",
-                        justifyContent: 'start',
-                        alignItems: 'center'
-                    }}>
-                        {GlobeContainer()}
-                    </div>
+                {SiteContent()}
+                {Footer()}
             </ThemeProvider>
         </>
     )
 }
+
 
 export default App
