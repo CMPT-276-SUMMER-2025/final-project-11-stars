6a4d72e79a6ecf97cc7cdde63b987ad5a5168cf9
bidishaaroy <bidisharoybrn@gmail.com>
2025-08-07 01:25:50 -0700

    Update events.tests.js

--- src/model/tests/api1-tests/events.tests.js
+++ src/model/tests/api1-tests/events.tests.js
@@ -1,92 +1,34 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g = Object.create((typeof Iterator === "function" ? Iterator : Object).prototype);
-    return g.next = verb(0), g["throw"] = verb(1), g["return"] = verb(2), typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (g && (g = 0, op[0] && (_ = 0)), _) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+const eventsC = require("../../events");
+const axios = require("axios");
+
+describe("events tests", () => {
+  test("loadNewsFeedData should fetch same number of events and match headlines", async () => {
+    const eventsFromModel = await eventsC.loadNewsFeedData();
+
+    const URL = "https://lldev.thespacedevs.com/2.3.0/events/?limit=3&?ordering=-last_updated";
+    const result = await axios.get(URL);
+    const eventsFromAPI = result.data.results;
+
+    let errorMessage = "";
+
+    if (eventsFromAPI.length !== eventsFromModel.length) {
+      errorMessage = `FAIL, the number of fetched events are not equal. Lengths were ${eventsFromModel.length} and ${eventsFromAPI.length}`;
+      console.log(errorMessage);
+      throw new Error(errorMessage);
     }
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var eventsC = require("../../events");
-var axios_1 = require("axios");
-function testLoadNews() {
-    return __awaiter(this, void 0, void 0, function () {
-        var eventsFromModel, URL, result, eventsFromAPI, errorMessage, i, eventModel, eventAPI;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0: return [4 /*yield*/, eventsC.loadNewsFeedData()];
-                case 1:
-                    eventsFromModel = _a.sent();
-                    URL = "https://lldev.thespacedevs.com/2.3.0/events/?limit=3&?ordering=-last_updated";
-                    return [4 /*yield*/, axios_1.default.get(URL)];
-                case 2:
-                    result = (_a.sent());
-                    eventsFromAPI = result.data.results;
-                    errorMessage = "";
-                    if (eventsFromAPI.length != eventsFromModel.length) {
-                        errorMessage = "FAIL, the number of feteched events are not equal. Lengths were ".concat(eventsFromModel.length, " and ").concat(eventsFromAPI.length);
-                        console.log(errorMessage);
-                        return [2 /*return*/, false];
-                    }
-                    for (i = 0; i < eventsFromModel.length; i++) {
-                        eventModel = eventsFromModel[i];
-                        eventAPI = eventsFromAPI[i];
-                        if (eventModel.headline != eventAPI.name) {
-                            errorMessage = "The headlines of events do not match. Headlines were \"".concat(eventModel.event.name, "\" and \"").concat(eventAPI.headline, "\"");
-                            console.log(errorMessage);
-                            return [2 /*return*/, false];
-                        }
-                    }
-                    return [2 /*return*/, true];
-            }
-        });
-    });
-}
-function main() {
-    return __awaiter(this, void 0, void 0, function () {
-        var _a, _b;
-        return __generator(this, function (_c) {
-            switch (_c.label) {
-                case 0:
-                    console.log("events tests");
-                    // each function returns T/F, can remove these print statements if necessary
-                    _b = (_a = console).log;
-                    return [4 /*yield*/, testLoadNews()];
-                case 1:
-                    // each function returns T/F, can remove these print statements if necessary
-                    _b.apply(_a, [_c.sent()]);
-                    return [2 /*return*/];
-            }
-        });
-    });
-}
-main();
+
+    for (let i = 0; i < eventsFromModel.length; i++) {
+      const eventModel = eventsFromModel[i];
+      const eventAPI = eventsFromAPI[i];
+
+      if (eventModel.headline !== eventAPI.name) {
+        errorMessage = `The headlines of events do not match. Headlines were "${eventModel.event.name}" and "${eventAPI.headline}"`;
+        console.log(errorMessage);
+        throw new Error(errorMessage);
+      }
+    }
+
+    // If we reach here, the test has passed
+    expect(true).toBe(true);
+  });
+});
