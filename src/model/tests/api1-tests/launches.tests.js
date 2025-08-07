889fe4d6b08d5117683f885b998747ea28f80e5f
Anton-Pan <91840673+Anton-Pan@users.noreply.github.com>
2025-08-07 13:02:43 -0700

    fixes another launch test

--- src/model/tests/api1-tests/launches.tests.js
+++ src/model/tests/api1-tests/launches.tests.js
@@ -77,20 +77,66 @@ describe("Launches Model Tests", () => {
 
     const expectedBasicLaunchDataArray = [
       {
-        id: "86139b24-aed8-47b0-a385-5ed28cca6409",
-        name: "Falcon 9 Block 5",
-        lng: -120.611,
-        lat: 34.632,
+        id: 'e2d43a8e-6b36-40ac-a671-de35edca331a',
+        name: 'Long March 4B',
+        lng: 111.589567,
+        lat: 38.863128
       },
       {
-        id: "59426ed2-57ff-4f61-8f62-9794b6dbb9ad",
-        name: "Falcon 9 Block 5",
-        lng: -80.57735736,
-        lat: 28.56194122,
+        id: '0f63b33d-1a83-4eb2-8c2e-d4157bf71230',
+        name: 'Falcon 9 Block 5',
+        lng: -80.60428186,
+        lat: 28.60822681
       },
+      {
+        id: 'a059955d-baf8-436d-9a2f-e4746be5a636',
+        name: 'Falcon 9 Block 5',
+        lng: -80.57735736,
+        lat: 28.56194122
+      },
+      {
+        id: '2aa76cd0-b1e0-4826-827e-1e69d1cc387d',
+        name: 'Falcon 9 Block 5',
+        lng: -120.611,
+        lat: 34.632
+      },
+      {
+        id: 'f7b56f6c-4ff8-4569-bacb-d752fece8450',
+        name: 'Atlas V 551',
+        lng: -80.58303644,
+        lat: 28.58341025
+      },
+      {
+        id: 'd4ab0770-45ea-4dae-b44b-b0be66c6a71d',
+        name: 'Long March 3B/E',
+        lng: 102.026751,
+        lat: 28.245564
+      },
+      {
+        id: '172111f8-35c3-413e-9f6e-06676a462895',
+        name: 'Falcon 9 Block 5',
+        lng: -80.60428186,
+        lat: 28.60822681
+      },
+      {
+        id: '8db4129c-513e-4258-bbde-c66f63c0c7ee',
+        name: 'Electron',
+        lng: 177.864469,
+        lat: -39.262833
+      },
+      {
+        id: '86139b24-aed8-47b0-a385-5ed28cca6409',
+        name: 'Falcon 9 Block 5',
+        lng: -120.611,
+        lat: 34.632
+      },
+      {
+        id: '59426ed2-57ff-4f61-8f62-9794b6dbb9ad',
+        name: 'Falcon 9 Block 5',
+        lng: -80.57735736,
+        lat: 28.56194122
+      }
     ];
-
-    const expectedDetailedLaunchDataArray = expect.any(Array); // You can do deep comparison if needed
 
     let responseBasicLaunchDataArray = [];
     let responseDetailedLaunchDataArray = [];
@@ -111,6 +157,6 @@ describe("Launches Model Tests", () => {
     );
 
     expect(responseBasicLaunchDataArray).toEqual(expectedBasicLaunchDataArray);
-    expect(responseDetailedLaunchDataArray.length).toBeGreaterThan(0); // just ensure it got something
+    expect(responseDetailedLaunchDataArray.length).toBeGreaterThan(0); // deep comparison doesn't metter here as the basic data comes from the detailed data
   });
 });
